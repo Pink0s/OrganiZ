258cfb924dac8fb55eaf5856168c9478850e68a2
habert_j <habert_j@etna-alternance.net>
2024-12-12 18:25:17 +0100

    feat(migrations): autodeploy database schema

--- organiz_api/src/configurations/typeorm_config.ts
+++ organiz_api/src/configurations/typeorm_config.ts
@@ -1,6 +1,11 @@
 import { DynamicModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UserAccount } from '../user_accounts/entities/user_account.entity';
+import { Task } from '../tasks/entities/task.entity';
+import { Category } from '../categories/entities/category.entity';
+import { Status } from '../statuses/entities/status.entity';
+import { Project } from '../projects/entities/project.entity';
 
 export const typeorm_config: DynamicModule = TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
@@ -14,7 +19,7 @@ export const typeorm_config: DynamicModule = TypeOrmModule.forRootAsync({
     username: configService.get<string>('DATABASE_USERNAME'),
     password: configService.get<string>('DATABASE_PASSWORD'),
     database: configService.get<string>('DATABASE_NAME'),
-    entities: [],
+    entities: [UserAccount, Task, Category, Status, Task, Project],
     synchronize: true,
   }),
 });
