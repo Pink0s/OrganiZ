645fe7a40eb822c1e8b17b2d47da3271535937c4
habert_j <habert_j@etna-alternance.net>
2024-12-12 18:23:54 +0100

    feat(migrations): task entity

---
+++ organiz_api/src/tasks/entities/task.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Status } from '../../statuses/entities/status.entity';
+import { Project } from '../../projects/entities/project.entity';
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 35, nullable: false })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @ManyToOne(() => Status, (status: Status) => status.id, {})
+  @JoinColumn({ name: 'status_id' })
+  status: Status;
+
+  @ManyToOne(() => Project, (project: Project) => project.id, {})
+  @JoinColumn({ name: 'project_id' })
+  project: Project;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+}
